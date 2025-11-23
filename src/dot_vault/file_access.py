90f1077b16463cfdbe5c414522e11c4bb9f92579
maromei <maromei@proton.me>
2025-11-23 16:02:32 +0100

    Added a safe `mkdir` function and added functions for creating library paths at the same time as getting them.

--- src/dot_vault/file_access.py
+++ src/dot_vault/file_access.py
@@ -3,10 +3,16 @@ import platform
 from pathlib import Path
 
 from returns.maybe import Maybe
-from returns.result import Failure, Result, Success
+from returns.result import Failure, Result, Success, safe
 
 from dot_vault.constants import LIB_NAME
 from dot_vault.xdg_directories import XDGUserDirectories
+
+
+@safe(exceptions=(OSError,))
+def mkdir(path: Path, parents: bool = False, exist_ok: bool = False) -> Path:
+    path.mkdir(parnest=parents, exist_ok=exist_ok)
+    return Path
 
 
 class CouldNotDetermineHostname(Exception):
@@ -29,6 +35,11 @@ def get_dotfile_library_path() -> Path:
     return data_path / LIB_NAME
 
 
+def get_and_create_dotfile_library_path() -> Result[Path, OSError]:
+    libpath: Path = get_dotfile_library_path()
+    return mkdir(libpath, parents=True, exist_ok=True)
+
+
 def get_local_dotfile_library_path(
     user: str | None = None, host: str | None = None
 ) -> Result[Path, CouldNotDetermineHostname]:
@@ -41,4 +52,13 @@ def get_local_dotfile_library_path(
         return get_dotfile_library_path() / username / host
 
     libpath: Result[Path, CouldNotDetermineHostname] = hostname.map(generate_path)
+    return libpath
+
+
+def get_and_create_local_dotfile_library_path(
+    user: str | None = None, host: str | None = None
+) -> Result[Path, OSError | CouldNotDetermineHostname]:
+    libpath: Result[Path, OSError | CouldNotDetermineHostname]
+    libpath = get_local_dotfile_library_path(user=user, host=host)
+    libpath = libpath.bind(lambda x: mkdir(x, parents=True, exist_ok=True))
     return libpath
