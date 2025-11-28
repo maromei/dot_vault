16680dd3513e073e0febb56e2652f84cba8b626a
maromei <maromei@proton.me>
2025-11-28 20:14:54 +0100

    Implemented `FileFilter` as an object to hold filter specifications.
    - Switched `filter_sources_on_file()` to use the new `FileFilter` object.
    - Switched `filter_files_on_config()` to use the new `FileFilter` object.
    - Renamed `__combine_possible_scalar_and_list` to
      `_combine_possible_scalar_and_list`
      - (Removed one leading underscore)
      - Avoids Name mangeling.
    - Adjusted the Tests for the change.

--- src/dot_vault/filter.py
+++ src/dot_vault/filter.py
@@ -1,6 +1,7 @@
 """Contain function for filtering / querying sets of files specified in the config."""
 
 import logging
+from dataclasses import dataclass
 
 from returns.maybe import Maybe, Nothing, Some
 
@@ -9,7 +10,7 @@ from dot_vault.config_model import Config, FileIdentity
 LOGGER = logging.getLogger(__name__)
 
 
-def __combine_possible_scalar_and_list[T](
+def _combine_possible_scalar_and_list[T](
     scalar: T | None, vec: list[T] | None
 ) -> Maybe[list[T]]:
     """Combines an optional scalar and list.
@@ -51,50 +52,79 @@ def __combine_possible_scalar_and_list[T](
     )
 
 
-def filter_sources_on_file(
-    file: FileIdentity,
-    user: str | None = None,
-    host: str | None = None,
-    source: str | None = None,
-    source__in: list[str] | None = None,
-) -> FileIdentity:
+@dataclass
+class FileFilter:
+    """Is used to specify keywords for functions used to filter files.
+
+    See f.e. [`filter_source_on_file`][] or [`filter_files_on_config`][].
+    """
+
+    #: Username to check if a [`FileSource`][] is available for the given
+    #: user-/hostname combination.
+    user: str | None = None
+
+    #: Hostname to check if a [`FileSource`][] is available for the given
+    #: user-/hostname combination.
+    host: str | None = None
+
+    file: str | None = None  #: [`FileIdentity.name`][] to filter for.
+
+    #: [`FileSource.name`][`dot_vault.config_model.FileSource.name`] to filter for.
+    source: str | None = None
+
+    file__in: list[str] | None = None  #: List of `file`s to filter for.
+    source__in: list[str] | None = None  #: List of `source`s to filter for.
+
+    #: Should a [`FileIdentity`][] be dropped if the name is
+    #: matched, but none of the [`FileSource`][dot_vault.config_model.FileSource]s are?
+    remove_empty_identities: bool = True
+
+    @property
+    def filenames(self) -> Maybe[list[str]]:
+        """Joins `file` and `file__in` into a single list.
+
+        Returns:
+            [`Some()`][] of a list of values specified in `file` and/or `file__in`.
+            If both fields are `None`, [`Nothing`][] is returned.
+        """
+        return _combine_possible_scalar_and_list(self.file, self.file__in)
+
+    @property
+    def sourcenames(self) -> Maybe[list[str]]:
+        """Joins `source` and `source__in` into a single list.
+
+        Returns:
+            [`Some()`][] of a list of values specified in `source` and/or `source__in`.
+            If both fields are `None`, [`Nothing`][] is returned.
+        """
+        return _combine_possible_scalar_and_list(self.source, self.source__in)
+
+
+def filter_sources_on_file(file: FileIdentity, file_filter: FileFilter) -> FileIdentity:
     """Filter the sources for a `FileIdentity`.
 
     Filter conditions for user-/hostname filtering will be 'and'-ed
     together with the source name condition.
 
     Args:
-        user: If not `None`, each
-            [`FileSource`][dot_vault.config_model.FileSource] will be checked
-            whether the `user` and `host` combination is allowed.
-            See
-            [`FileSource.is_allowed()`][dot_vault.config_model.FileSource.is_allowed].
-        host: If not `None`, each
-            [`FileSource`][dot_vault.config_model.FileSource] will be checked
-            whether the `user` and `host` combination is allowed.
-            See
-            [`FileSource.is_allowed()`][dot_vault.config_model.FileSource.is_allowed].
-        source: If not `None`, only sources with the given name will be returned.
-        source__in: If not `None`, only sources with names in the list will be returned.
+        file: [`FileIdentity`][] which will be filtered.
+        file_filter: Contains filter definitions to apply.
 
     Return:
         A deepcopy of the `file` with the `sources` filtered based on the given rules.
     """
 
     file = file.model_copy(deep=True)
-    sourcenames: Maybe[list[str]] = __combine_possible_scalar_and_list(
-        source, source__in
-    )
 
     source_list = []
     for file_source in file.sources:
-        maybe_keep_source_on_name: Maybe[bool] = sourcenames.do(
-            file_source.name in name_list for name_list in sourcenames
+        maybe_keep_source_on_name: Maybe[bool] = Maybe.do(
+            file_source.name in name_list for name_list in file_filter.sourcenames
         )
 
         keep_source_on_name: bool = maybe_keep_source_on_name.value_or(True)
         keep_source_on_userhost: bool = file_source.is_allowed(
-            username=user, hostname=host
+            username=file_filter.user, hostname=file_filter.host
         )
 
         if keep_source_on_name and keep_source_on_userhost:
@@ -104,47 +134,28 @@ def filter_sources_on_file(
     return file
 
 
-def filter_files_on_config(
-    config: Config,
-    user: str | None = None,
-    host: str | None = None,
-    file: str | None = None,
-    source: str | None = None,
-    file__in: list[str] | None = None,
-    source__in: list[str] | None = None,
-    remove_empty_identities: bool = True
-) -> Config:
+def filter_files_on_config(config: Config, file_filter: FileFilter) -> Config:
     """Filter [`FileIdentity`]s and [`FileSource`]s.
 
     Each model is deepcopied, so none of the existing inputs are
     modified in place.
 
     Args:
-        user: Username to check if a [`FileSource`] is available for the given
-            user-/hostname combination.
-        host: Hostname to check if a [`FileSource`] is available for the given
-            user-/hostname combination.
-        file: [`FileIdentity.name`] to filter for.
-        source: [`FileSource.name`] to filter for.
-        file__in: List of [`FileIdentity.name`]s to filter for.
-        source__in: List of [`FileSource.name`]s to filter for.
-        remove_empty_identities: Should a [`FileIdentity`] be dropped if the name is
-            matched, but none of the [`FileSource`]s are?
+        config: [`Config`][] which will be filtered.
+        file_filter: Contains filter definitions to apply.
 
     Returns:
         [`Config`] with a filtered [`files`][Config.file] list.
     """
 
     config = config.model_copy(deep=True)
-    filenames: Maybe[list[str]] = __combine_possible_scalar_and_list(file, file__in)
 
     file_list = list[FileIdentity]()
     for file_identity in config.files:
-
         # fmt: off
-        maybe_keep_file: Maybe[bool] = filenames.do(
+        maybe_keep_file: Maybe[bool] = Maybe.do(
             file_identity.name in name_list
-            for name_list in filenames
+            for name_list in file_filter.filenames
         )
         # fmt: on
 
@@ -153,15 +164,11 @@ def filter_files_on_config(
             continue
 
         filtered_file: FileIdentity = filter_sources_on_file(
-            file=file_identity,
-            user=user,
-            host=host,
-            source=source,
-            source__in=source__in,
+            file=file_identity, file_filter=file_filter
         )
 
         num_sources: int = len(filtered_file.sources)
-        if remove_empty_identities and num_sources == 0:
+        if file_filter.remove_empty_identities and num_sources == 0:
             continue
 
         file_list.append(filtered_file)
--- src/tests/test_filter.py
+++ src/tests/test_filter.py
@@ -6,21 +6,25 @@ from returns.maybe import Maybe, Nothing, Some
 from returns.result import Result
 
 from dot_vault.config_model import Config, ParseConfigError
-from dot_vault.filter import __combine_possible_scalar_and_list, filter_files_on_config
+from dot_vault.filter import (
+    FileFilter,
+    _combine_possible_scalar_and_list,
+    filter_files_on_config,
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
 def test_combine_possible_scalar_and_list():
-    assert __combine_possible_scalar_and_list(1, None) == Some([1])
-    assert __combine_possible_scalar_and_list(None, [2, 3]) == Some([2, 3])
-    assert __combine_possible_scalar_and_list(1, [2, 3]) == Some([1, 2, 3])
-    assert __combine_possible_scalar_and_list(None, None) == Nothing
+    assert _combine_possible_scalar_and_list(1, None) == Some([1])
+    assert _combine_possible_scalar_and_list(None, [2, 3]) == Some([2, 3])
+    assert _combine_possible_scalar_and_list(1, [2, 3]) == Some([1, 2, 3])
+    assert _combine_possible_scalar_and_list(None, None) == Nothing
 
 
 def test_combine_possible_scalar_and_list_mutability():
     list_: list[int] = [2, 3]
-    result: Maybe[list[int]] = __combine_possible_scalar_and_list(1, list_)
+    result: Maybe[list[int]] = _combine_possible_scalar_and_list(1, list_)
     assert list_ == [2, 3]
     assert result == Some([1, 2, 3])
 
@@ -28,7 +32,7 @@ def test_combine_possible_scalar_and_list_mutability():
 def test_combine_possible_scalar_and_list_nested_mutability():
     list_: list[list[str]] = [["2.1", "2.2"], ["3.1", "3.2"]]
     scalar: list[str] = ["1.1", "1.2"]
-    result: Maybe[list[list[str]]] = __combine_possible_scalar_and_list(scalar, list_)
+    result: Maybe[list[list[str]]] = _combine_possible_scalar_and_list(scalar, list_)
     assert list_ == [["2.1", "2.2"], ["3.1", "3.2"]]
     assert result == Some([["1.1", "1.2"], ["2.1", "2.2"], ["3.1", "3.2"]])
 
@@ -70,7 +74,8 @@ def test_config_filter(tmp_path: Path):
     config_result: Result[Config, ParseConfigError] = Config.from_json_str(json_str)
     config: Config = config_result.unwrap()
 
-    filtered_config: Config = filter_files_on_config(config, user="pommy")
+    filter_specs: FileFilter = FileFilter(user="pommy")
+    filtered_config: Config = filter_files_on_config(config, filter_specs)
     expected_filenames = ["file1", "file2"]
     expected_sourcenames = ["source1-1", "source1-3", "sources2-1"]
 
@@ -83,7 +88,8 @@ def test_config_filter(tmp_path: Path):
     ] == expected_sourcenames
     # fmt: on
 
-    filtered_config = filter_files_on_config(config, user="pommy", file="file1")
+    filter_specs = FileFilter(user="pommy", file="file1")
+    filtered_config = filter_files_on_config(config, filter_specs)
     expected_filenames = ["file1"]
     expected_sourcenames = ["source1-1", "source1-3"]
 
@@ -97,7 +103,8 @@ def test_config_filter(tmp_path: Path):
     # fmt: on
 
     # shoulde be empty due to empty {} definition on only_on
-    filtered_config = filter_files_on_config(config, source="source1-2")
+    filter_specs = FileFilter(source="source1-2")
+    filtered_config = filter_files_on_config(config, filter_specs)
     expected_filenames = []
     expected_sourcenames = []
 
@@ -110,7 +117,8 @@ def test_config_filter(tmp_path: Path):
     ] == expected_sourcenames
     # fmt: on
 
-    filtered_config = filter_files_on_config(config, source="source1-1")
+    filter_specs = FileFilter(source="source1-1")
+    filtered_config = filter_files_on_config(config, filter_specs)
     expected_filenames = ["file1"]
     expected_sourcenames = ["source1-1"]
 
@@ -123,9 +131,8 @@ def test_config_filter(tmp_path: Path):
     ] == expected_sourcenames
     # fmt: on
 
-    filtered_config = filter_files_on_config(
-        config, source="source1-1", remove_empty_identities=False
-    )
+    filter_specs = FileFilter(source="source1-1", remove_empty_identities=False)
+    filtered_config = filter_files_on_config(config, filter_specs)
     expected_filenames = ["file1", "file2"]
     expected_sourcenames = ["source1-1"]
 
